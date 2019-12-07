ada85d048182a7581a7148a0a14b4130c504bc45
mike_neck <jkrt3333@gmail.com>
2019-12-07 10:25:08 +0900

    create-new-author ユースケースを作成した

---
+++ book-management-system/domains/src/main/kotlin/com/example/book/usecases/create-new-author.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.book.usecases
+
+import com.example.book.Cause
+import com.example.book.Reason
+import com.example.book.ResultEx
+import com.example.book.ResultEx.Companion.asResult
+import com.example.book.domains.Author
+import com.example.book.domains.AuthorName
+import com.example.book.repository.AuthorWriteRepository
+
+class CreateNewAuthor(private val authorWriteRepository: AuthorWriteRepository) {
+  fun apply(authorName: AuthorName): ResultEx<Reason, Author> =
+      authorWriteRepository.save(authorName)
+          .asResult { Cause.CONFLICT.with("failed to save author(${authorName.firstName.value},${authorName.lastName.value})") }
+}
