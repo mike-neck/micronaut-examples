44925a79524f9100c7012ace973cffa433f07a8e
mike_neck <jkrt3333@gmail.com>
2019-12-10 23:20:12 +0900

    JDBC/ID生成器 設定

---
+++ book-management-system/src/main/kotlin/com/example/ApplicationConfig.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example
+
+import com.example.book.ids.IdGen
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import org.seasar.doma.jdbc.Config
+import org.seasar.doma.jdbc.Naming
+import org.seasar.doma.jdbc.dialect.Dialect
+import org.seasar.doma.jdbc.dialect.H2Dialect
+import java.util.concurrent.atomic.AtomicLong
+import javax.inject.Inject
+import javax.inject.Singleton
+import javax.sql.DataSource
+
+@Factory
+class ApplicationConfig {
+
+  @Bean
+  fun idGen(): IdGen = object : IdGen {
+    val atomicLong: AtomicLong = AtomicLong(0)
+    override fun newLongId(): Long = atomicLong.incrementAndGet()
+  }
+}
+
+@Singleton
+class DatabaseConfig
+@Inject constructor(dataSource: DataSource): Config {
+
+  private val injectedDataSource: DataSource = dataSource
+
+  override fun getDataSource(): DataSource = injectedDataSource
+
+  override fun getNaming(): Naming = Naming.SNAKE_LOWER_CASE
+
+  override fun getDialect(): Dialect = h2Dialect
+
+  companion object {
+    val h2Dialect: Dialect = H2Dialect()
+  }
+} 
