e7b2e7e7d110719ce2318ec8aef3d37a7303fc06
mike_neck <jkrt3333@gmail.com>
2019-12-10 23:31:35 +0900

    controller を移動

--- book-management-system/src/main/kotlin/com/example/author/AuthorController.kt
+++ book-management-system/src/main/kotlin/com/example/author/AuthorController.kt
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example
+package com.example.author
 
+import com.example.Logger
 import com.example.book.ResultEx
 import com.example.book.attributes.AuthorFirstName
 import com.example.book.attributes.AuthorLastName
