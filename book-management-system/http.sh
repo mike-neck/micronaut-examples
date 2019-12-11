c674347d7920d63af6ee9bf14d97983dbd0e7702
mike_neck <jkrt3333@gmail.com>
2019-12-11 09:58:40 +0900

    テスト()用

---
+++ book-management-system/http.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "curl -i http://localhost:8080/authors/1"
+curl -i http://localhost:8080/authors/1
+echo ""
+echo ""
+
+echo "curl -i http://localhost:8080/authors -X POST -d 'firstName=三成' -d 'lastName=石田'"
+curl -i http://localhost:8080/authors -X POST -d 'firstName=三成' -d 'lastName=石田'
+echo ""
+echo ""
+
+echo "curl -i http://localhost:8080/authors/1"
+curl -i http://localhost:8080/authors/1
+echo ""
+echo ""
+
+echo "curl -i http://localhost:8080/authors/1/books -X POST -d 'name=罪と罰' -d 'price=3200' -d 'publish=2019-12-11T12:34:56.789Z'"
+curl -i http://localhost:8080/authors/1/books -X POST -d 'name=罪と罰' -d 'price=3200' -d 'publish=2019-12-11T12:34:56.789Z'
+echo ""
+echo ""
+
+echo "curl -i http://localhost:8080/authors/1/books/1"
+curl -i http://localhost:8080/authors/1/books/1
+echo ""
+echo ""
+
+echo "curl -i http://localhost:8080/books/1"
+curl -i http://localhost:8080/books/1
+echo ""
+echo ""
