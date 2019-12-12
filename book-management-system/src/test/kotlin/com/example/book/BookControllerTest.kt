b68a6fccdfa434535c6fa16439c5b671c9099dcf
mike_neck <jkrt3333@gmail.com>
2019-12-12 23:25:55 +0900

    GET /books/{bookId} のテストを追加した

---
+++ book-management-system/src/test/kotlin/com/example/book/BookControllerTest.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.book
+
+import com.example.IntegrationBehaviorSpec
+import com.example.book.ids.IdGen
+import com.example.json.BookJson
+import com.example.sql
+import io.kotlintest.shouldBe
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.test.annotation.MicronautTest
+import javax.sql.DataSource
+
+@MicronautTest
+class BookControllerGetBookTest(
+    override val dataSource: DataSource,
+    private val idGen: IdGen,
+    @Client("/books") private val client: RxHttpClient
+) : IntegrationBehaviorSpec ({
+
+  given("no books saved") {
+    `when`("""curl http://example.com/books/1""") {
+      val result = client.toBlocking().runCatching { exchange<BookJson>("/1") }
+      then("http status is 404") {
+        val e = result.exceptionOrNull() ?: throw AssertionError("expected exception existing, but not")
+        val httpException = e as? HttpClientResponseException
+            ?: throw AssertionError("expected exception to be instance of HttpClientResponseException, but ${e.javaClass}")
+        httpException.status shouldBe HttpStatus.NOT_FOUND
+      }
+    }
+  }
+
+  given("some books saved") {
+    val authorId = idGen.newLongId()
+    val bookId = idGen.newLongId()
+    dataSource.connection.use { connection -> 
+      //language=sql
+      connection.sql("""
+        insert into AUTHORS (ID, FIRST_NAME, LAST_NAME)
+        VALUES ( $authorId, '三成', '石田' )
+      """.trimIndent())
+      //language=sql
+      connection.sql("""
+        insert into BOOKS (ID, NAME, PRICE, PUBLICATION_DATE)
+        VALUES ( $bookId, '罪と罰', 3200, '2019-12-11 12:34:56.789' )
+      """.trimIndent())
+      //language=sql
+      connection.sql("""
+        insert into WRITINGS (BOOK_ID, AUTHOR_ID) VALUES ( $bookId, $authorId )
+      """.trimIndent())
+    }
+    `when`("""curl http://example.com/books/$bookId""") {
+      val result = client.toBlocking()
+          .exchange<String>("/$bookId")
+      then("http status is 200") {
+        result.status shouldBe HttpStatus.OK
+      }
+    }
+  }
+})
